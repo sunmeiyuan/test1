0ed6b0f020a45e80e9fc0a4a751721bd57c6ff9c
sunmeiyuan <469112379@qq.com>
2018-10-07 14:27:10 +0800

    qqqq

--- MyTest1/src/com/psz/Test2.java
+++ MyTest1/src/com/psz/Test2.java
@@ -11,8 +11,7 @@ public class Test2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub6666666
-//4444
+		// TODO Auto-generated method stub11111
 	}
 
 }
