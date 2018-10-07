f5947e74b503e317c02f09a71f2e52cdbdc5ac4f
sunmeiyuan <469112379@qq.com>
2018-10-07 14:36:49 +0800

    wwww

--- MyTest1/src/com/psz/Test2.java
+++ MyTest1/src/com/psz/Test2.java
@@ -11,7 +11,7 @@ public class Test2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub11111
+		// TODO Auto-generated method stub11111333333
 	}
 
 }
--- MyTest1/src/com/psz/Test3.java
+++ MyTest1/src/com/psz/Test3.java
@@ -14,6 +14,7 @@ public class Test3 {
 		// TODO Auto-generated method stub
 //2222
 		System.out.println("11111111");
+		System.out.println("33333333");
 	}
 
 }
