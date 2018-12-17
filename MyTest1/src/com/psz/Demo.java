fd04b2bf67c2dd884a3a52815962523a2cff6bc8
sunmeiyuan <469112379@qq.com>
2018-12-17 22:55:57 +0800

    demo

---
+++ MyTest1/src/com/psz/Demo.java
@@ -0,0 +1,20 @@
+package com.psz;
+
+/**
+ * 
+ * @author psz
+ * CreateDate:2018年12月17日
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("demo");
+		
+
+	}
+
+}
